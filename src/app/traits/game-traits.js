fb1d207e744ffa8fc32466fab5d87bc2f553075a
nybatista <nybatista@aol.com>
2023-09-19 04:25:02 -0400

    game traits is working well

--- src/app/traits/game-traits.js
+++ src/app/traits/game-traits.js
@@ -12,6 +12,27 @@ export class GameTraits extends SpyneTrait {
     super(context, traitPrefix);
   }
 
+  static game$GetMoveBtnText(num=0){
+    return num === 0 ? "Go to game start" : `Go to move #${num}`;
+  }
+  static game$GetStatusText(state={isWinner:false, nextSquareVal:"X"}){
+    return state.isWinner ? `Winner: ${state.currentSquareVal}` : `Next player: ${state.nextSquareVal}`;
+  }
+
+  static game$CreateMoveItem(e={payload:{moveNum: 0}}){
+    const {moveNum} = e.payload;
+    const text = this.game$GetMoveBtnText(moveNum);
+    this.appendView(new TicTacToeMoveBtn( {text, moveNum}), "ol");
+
+  }
+
+
+
+  static game$UpdateStatusText(e={payload:{isWinner:false, nextSquareVal:"X"}}, props=this.props){
+    this.props.el$('.status').el.innerText = this.game$GetStatusText(e.payload);
+  }
+
+
   static game$CreateStateMachine(movesArr=[]){
 
     let _movesArr = movesArr;
@@ -28,13 +49,13 @@ export class GameTraits extends SpyneTrait {
       set square(n=0){
           _movesArr.splice(_lastMove, _movesArr.length, n);
           _lastMove = _movesArr.length;
-        console.log("moves arr ",{_movesArr,n});
+        //console.log("moves arr ",{_movesArr,n});
 
       }
 
       set move(n=0){
         _lastMove = parseInt(n);
-        console.log('last move is ',_lastMove);
+        //console.log('last move is ',_lastMove);
       }
 
       get squares(){
@@ -45,17 +66,23 @@ export class GameTraits extends SpyneTrait {
         //const squaresArr = _movesArr.toSpliced(_lastMove, _movesArr.length);
         const squaresArr = _movesArr.toSpliced(_lastMove, _movesArr.length).reduce(reduceToXO, []);
 
-        console.log('squares arr is ',{_lastMove, squaresArr, _movesArr})
+        //console.log('squares arr is ',{_lastMove, squaresArr, _movesArr})
         return squaresArr;
         //return squaresArr.reduce(reduceToXO, []);
       }
 
 
       get state(){
-
+         const {squares} = this;
+         const winner = this.calculateWinner();
+         const isWinner = winner !== undefined;
+         const nextSquareVal = xoFn(_lastMove);
+         const statusText = isWinner ? `Winner: ${winner}` : `Next player: ${nextSquareVal}`;
+         const moveBtnText =  _lastMove === 0 ? "Go to game start" : `Go to move #${_lastMove}`;
+        return {squares, winner, isWinner, nextSquareVal, statusText, moveBtnText};
       }
 
-      calculateWinner(){
+      calculateWinner(xoArr=this.squares){
         const lines = [
           [0, 1, 2],
           [3, 4, 5],
@@ -75,6 +102,8 @@ export class GameTraits extends SpyneTrait {
         const winner = lines.reduce(reducer, undefined);
 
         console.log("winner is ",winner);
+
+        return winner;
 
       }
 
--- src/tests/unit-tests/game-traits.test.js
+++ src/tests/unit-tests/game-traits.test.js
@@ -28,13 +28,116 @@ describe('should test game traits', () => {
       gameState.square = 7;
       gameState.square = 2;
 
-      console.log("game state is ",gameState.squares);
 
     })
 
 
 
-  })
 
+
+
+
+  });
+
+  describe('it should test game state', ()=>{
+    const gameState = GameTraits.game$CreateStateMachine( [0,4,1,5,2])
+
+
+    console.log("game state is ",gameState.state);
+
+
+    describe('it should test game status text',()=>{
+      const game = GameTraits.game$CreateStateMachine();
+
+        it('it should return default status on zero moves', ()=>{
+          const defaultState = game.state;
+          expect(defaultState.squares).to.deep.eq([]);
+          expect(defaultState.winner).to.eq(undefined);
+          expect(defaultState.isWinner).to.be.false;
+          expect(defaultState.statusText).to.eq('Next player: X');
+          expect(defaultState.moveBtnText).to.eq('Go to game start');
+
+          //console.log("new game is ",defaultState);
+          return true;
+        })
+
+        it('it should return X on even moves',()=>{
+          game.square=2;
+
+          const xState = game.state;
+          expect(xState.squares).to.deep.eq([undefined, undefined, 'X']);
+          expect(xState.winner).to.eq(undefined);
+          expect(xState.isWinner).to.be.false;
+          expect(xState.statusText).to.eq('Next player: O');
+          expect(xState.moveBtnText).to.eq('Go to move #1');
+
+          //console.log("x game is ",xState);
+
+          return true;
+
+        })
+
+        it('it should return O on odd moves',()=>{
+          game.square=4;
+          const oState = game.state;
+          expect(oState.squares).to.deep.eq([undefined, undefined, 'X', undefined, 'O']);
+          expect(oState.winner).to.eq(undefined);
+          expect(oState.isWinner).to.be.false;
+          expect(oState.statusText).to.eq('Next player: X');
+          expect(oState.moveBtnText).to.eq('Go to move #2');
+
+          //console.log("o game is ",oState);
+
+
+
+        })
+
+        it('it should return winner when winner param is defined ',()=>{
+          game.square=0;
+          game.square=5;
+          game.square=1;
+
+
+          const xWinner = game.state;
+          expect(xWinner.squares).to.deep.eq(['X', 'X', 'X', undefined, 'O', 'O']);
+          expect(xWinner.winner).to.eq('X');
+          expect(xWinner.isWinner).to.be.true;
+          expect(xWinner.statusText).to.eq('Winner: X');
+          expect(xWinner.moveBtnText).to.eq('Go to move #5');
+
+          console.log("xWinner ",xWinner);
+
+        })
+
+      it('it should return winner O by going back several moves ',()=>{
+
+        game.move = 1;
+        game.square = 1;
+        game.square = 6;
+        game.square = 7;
+        game.square = 5;
+        game.square = 4;
+
+
+        const oWinner = game.state;
+        expect(oWinner.squares).to.deep.eq([undefined, 'O', 'X', undefined, 'O', 'X', 'X', 'O']);
+        expect(oWinner.winner).to.eq('O');
+        expect(oWinner.isWinner).to.be.true;
+        expect(oWinner.statusText).to.eq('Winner: O');
+        expect(oWinner.moveBtnText).to.eq('Go to move #6');
+
+        console.log("oWinner ",oWinner);
+
+      })
+
+
+      });
+
+
+
+
+
+
+  });
 
 });
